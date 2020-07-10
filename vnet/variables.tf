d982182dfb4cc43246f9bbb6f8612af64f6685fc
ang <andy.ng@digicatapult.org.uk>
2020-07-10 14:47:16 +0100

    comment edit

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -16,7 +16,7 @@ variable "owner" {
 
 locals {
   # Define resource names based on the following convention:
-  # {azurerm_resource_name_prefix}-{environment}-RESOURCE_TYPE
+  # {azurerm_resource_prefix}-{environment}-RESOURCE_TYPE
   azurerm_rg_name  = "${var.resource_prefix}-${var.env}-rg"
   azurerm_vnet_name = "${var.resource_prefix}-${var.env}-vnet"
 }

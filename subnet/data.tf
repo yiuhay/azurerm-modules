8468b6f8fa7124db24acddbc8ce68e46e683c6b0
ang <andy.ng@digicatapult.org.uk>
2020-07-10 15:17:43 +0100

    subnet data name update

--- subnet/data.tf
+++ subnet/data.tf
@@ -1,8 +1,8 @@
 data "azurerm_resource_group" "rg" {
-  name = local.azurerm_resource_group_name
+  name = local.azurerm_rg_name
 }
 
 data "azurerm_virtual_network" "vnet" {
-  name                = local.azurerm_virtual_network_name
+  name                = local.azurerm_vnet_name
   resource_group_name = data.azurerm_resource_group.rg.name
 }

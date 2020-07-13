e3450d7b6b7bb4e80306b2e254c7bb8e3136ab4f
ang <andy.ng@digicatapult.org.uk>
2020-07-13 12:06:07 +0100

    vnet module update

--- vnet/variables.tf
+++ vnet/variables.tf
@@ -14,6 +14,18 @@ variable "owner" {
   description = "tagging purposes"
 }
 
+variable "snet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  type        = list
+  default     = ["10.0.1.0/24"]
+}
+
+variable "snet_names" {
+  description = "A list of public subnets inside the vNet."
+  type        = list
+  default     = ["subnet1"]
+}
+
 locals {
   # Define resource names based on the following convention:
   # {azurerm_resource_prefix}-{environment}-RESOURCE_TYPE

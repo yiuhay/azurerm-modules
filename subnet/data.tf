3e1ea906148ebd6c4c451e461309f062fc679f54
ang <andy.ng@digicatapult.org.uk>
2020-07-13 11:59:18 +0100

    remove subnet module

--- subnet/data.tf
+++
@@ -1,8 +0,0 @@
-data "azurerm_resource_group" "rg" {
-  name = local.azurerm_rg_name
-}
-
-data "azurerm_virtual_network" "vnet" {
-  name                = local.azurerm_vnet_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-}
--- subnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "azurerm_subnet_name" {
-  description = "The name of the subnet created"
-  value       = local.azurerm_snet_name
-}
-
-output "azurerm_subnet_id" { 
-  description = "The id of the subnet created"
-  value       = azurerm_subnet.snet.id
-}

5ec97f84004d638863cd8d61e7d0c471fbd56ea0
ang <andy.ng@digicatapult.org.uk>
2020-07-15 11:54:16 +0100

    vm windows update'

--- vm_windows/providers.tf
+++ vm_windows/providers.tf
@@ -1,12 +1,6 @@
 provider "azurerm" {
   version = "=2.16.0"
 
-  features {}
-}
-
-provider "azurerm" {
-  version = "=2.16.0"
-  
   alias           = “keyvaultProvider”
   subscription_id = var.subscription_secret
 
